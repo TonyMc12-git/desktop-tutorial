84784a0106344b4b14b5e438944c0e8d93155c8e
TonyMc12-git <Tony.McIntosh12@gmail.com>
2026-04-12 17:59:24 +0100

    Fix phone words panel height and compact header

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "nine-letters-pwa-v4";
+const CACHE_NAME = "nine-letters-pwa-v5";
 
 const APP_ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./styles.css?v=20260412-noscrollfit",
-  "./game.js?v=20260412-noscrollfit",
+  "./styles.css?v=20260412-fixedwordspanel",
+  "./game.js?v=20260412-fixedwordspanel",
   "./data/preserved/size50-filtered-4to9-v2.txt?v=20260412-britishb",
   "./icons/icon.svg",
   "./icons/icon-maskable.svg"
