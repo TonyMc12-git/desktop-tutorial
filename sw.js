d766db2d1028a07462f1753cdfa2808906a809d3
TonyMc12-git <Tony.McIntosh12@gmail.com>
2026-04-12 22:01:02 +0100

    Improve contrast on dark purple panels

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "nine-letters-pwa-v20";
+const CACHE_NAME = "nine-letters-pwa-v21";
 
 const APP_ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./styles.css?v=20260412-purplepanel1",
-  "./game.js?v=20260412-purplepanel1",
+  "./styles.css?v=20260412-purplecontrast1",
+  "./game.js?v=20260412-purplecontrast1",
   "./data/preserved/size60-filtered-4to9-v2.txt?v=20260412-britishb",
   "./icons/icon.svg",
   "./icons/icon-maskable.svg"
