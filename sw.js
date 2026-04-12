b85f7fba9c365d38a11a7553507d3f3af9a6fb03
TonyMc12-git <Tony.McIntosh12@gmail.com>
2026-04-12 21:47:20 +0100

    Add lilac board and words backgrounds

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "nine-letters-pwa-v15";
+const CACHE_NAME = "nine-letters-pwa-v16";
 
 const APP_ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./styles.css?v=20260412-size60switch1",
-  "./game.js?v=20260412-size60switch1",
+  "./styles.css?v=20260412-lilacpanel1",
+  "./game.js?v=20260412-lilacpanel1",
   "./data/preserved/size60-filtered-4to9-v2.txt?v=20260412-britishb",
   "./icons/icon.svg",
   "./icons/icon-maskable.svg"
