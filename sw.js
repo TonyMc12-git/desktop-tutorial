d8131a7495da26dd1d66ad1395c3abef62dfc303
TonyMc12-git <Tony.McIntosh12@gmail.com>
2026-04-12 20:03:02 +0100

    Refine phone layout and lock found word chips

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "nine-letters-pwa-v6";
+const CACHE_NAME = "nine-letters-pwa-v7";
 
 const APP_ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./styles.css?v=20260412-lockedpanel2",
-  "./game.js?v=20260412-lockedpanel2",
+  "./styles.css?v=20260412-chipfix1",
+  "./game.js?v=20260412-chipfix1",
   "./data/preserved/size50-filtered-4to9-v2.txt?v=20260412-britishb",
   "./icons/icon.svg",
   "./icons/icon-maskable.svg"
