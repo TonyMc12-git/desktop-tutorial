b3eb1d8d9e813664f0a3704f8aadd94970541c24
TonyMc12-git <Tony.McIntosh12@gmail.com>
2026-04-12 21:52:07 +0100

    Darken lilac panels and strengthen found word green

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "nine-letters-pwa-v16";
+const CACHE_NAME = "nine-letters-pwa-v17";
 
 const APP_ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./styles.css?v=20260412-lilacpanel1",
-  "./game.js?v=20260412-lilacpanel1",
+  "./styles.css?v=20260412-lilacpanel2",
+  "./game.js?v=20260412-lilacpanel2",
   "./data/preserved/size60-filtered-4to9-v2.txt?v=20260412-britishb",
   "./icons/icon.svg",
   "./icons/icon-maskable.svg"
