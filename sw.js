65aacdd797f47acac9678edb3d5aa4ba161f8621
TonyMc12-git <Tony.McIntosh12@gmail.com>
2026-04-12 21:58:08 +0100

    Switch board background to dark purple

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "nine-letters-pwa-v19";
+const CACHE_NAME = "nine-letters-pwa-v20";
 
 const APP_ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./styles.css?v=20260412-lilacpanel4",
-  "./game.js?v=20260412-lilacpanel4",
+  "./styles.css?v=20260412-purplepanel1",
+  "./game.js?v=20260412-purplepanel1",
   "./data/preserved/size60-filtered-4to9-v2.txt?v=20260412-britishb",
   "./icons/icon.svg",
   "./icons/icon-maskable.svg"
