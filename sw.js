0f1f51f404a4c9223b245ab7b921b1c0aaf48d67
TonyMc12-git <Tony.McIntosh12@gmail.com>
2026-04-12 23:06:48 +0100

    Use full-height 5x10 phone word grid

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "nine-letters-pwa-v36";
+const CACHE_NAME = "nine-letters-pwa-v37";
 
 const APP_ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./styles.css?v=20260412-phonewordbox2",
-  "./game.js?v=20260412-phonewordbox2",
+  "./styles.css?v=20260412-phonewordbox3",
+  "./game.js?v=20260412-phonewordbox3",
   "./data/preserved/size60-filtered-4to9-v2.txt?v=20260412-britishb",
   "./icons/icon.svg",
   "./icons/icon-maskable.svg"
