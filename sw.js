58212b11963da67d887d46e70a234a8f0011a0e7
TonyMc12-git <Tony.McIntosh12@gmail.com>
2026-05-03 17:10:47 +0100

    Bump nine-letter app cache version

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "nine-letters-pwa-v41";
+const CACHE_NAME = "nine-letters-pwa-v42";
 
 const APP_ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./styles.css?v=20260418-blue1",
-  "./game.js?v=20260418-touch2",
+  "./styles.css?v=20260503-blue2",
+  "./game.js?v=20260503-touch3",
   "./data/preserved/size60-filtered-4to9-v2.txt?v=20260412-britishb",
   "./icons/icon.svg",
   "./icons/icon-maskable.svg"
